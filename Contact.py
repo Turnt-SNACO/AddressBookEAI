4e05e33a65744ab77bdd452192d0d85273097e70
Turnt-SNACO <janderson5@albany.edu>
2019-02-07 18:57:07 -0500

    added email

--- Contact.py
+++ Contact.py
@@ -1,6 +1,10 @@
+# Author: James Anderson
+# EAI Coding Challenge: Address Book
+
 #A container for contact information
 class Contact:
-    def __init__(self, name, address, phone_number):
+    def __init__(self, name, address='', phone_number='', email_address=''):
         self.name = name
         self.address = address
+        self.email_address = email_address
         self.phone_number = phone_number
