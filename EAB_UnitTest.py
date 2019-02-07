aad52091e46727516bff4ca1cac35ab6eb35b23c
Turnt-SNACO <janderson5@albany.edu>
2019-02-07 13:37:47 -0500

    added more unit tests

--- EAB_UnitTest.py
+++ EAB_UnitTest.py
@@ -29,8 +29,46 @@ class TestEabMethods(unittest.TestCase):
         self.assertEqual(eab.search_contact('Zulu Fakename').phone_number, '1-1-1')
         self.assertNotEqual(eab.search_contact('Zulu Fakename').phone_number, '123-456-7890')
         self.assertTrue(eab.delete_contact('Zulu Fakename'))
+    def test_list_contacts(self):
+        self.assertEqual(len(eab.list_contacts(100, 0)), 100)
+        self.assertNotEqual(eab.list_contacts(100,0), eab.list_contacts(100,1))
+        self.assertEqual(list(set(eab.list_contacts(100,0)) & set(eab.list_contacts(100,1))), [])
+        self.assertEqual(list(set(eab.list_contacts(100,0)) & set(eab.list_contacts(50,1))), [])
+        self.assertNotEqual(eab.list_contacts(20,0)[2].name, None)
+    def test_bad_data_types(self):
+        self.assertRaises(TypeError, eab.add_contact, 'string' , 2 , 3)
+        self.assertRaises(TypeError, eab.add_contact, 1 , 'string' , 3)
+        self.assertRaises(TypeError, eab.add_contact, 1 , 2 , 'string')
+        self.assertRaises(TypeError, eab.add_contact, 'string' , 'string' , 3)
+        self.assertRaises(TypeError, eab.add_contact, 'string' , 2 , 'string')
+        self.assertRaises(TypeError, eab.add_contact, 1 , 'string' , 'string')
+        self.assertRaises(TypeError, eab.delete_contact, 1)
+        self.assertRaises(TypeError, eab.delete_contact, 1.0)
+        self.assertRaises(TypeError, eab.update_contact, 'string' , 2 , 3)
+        self.assertRaises(TypeError, eab.update_contact, 1 , 'string' , 3)
+        self.assertRaises(TypeError, eab.update_contact, 1 , 2 , 'string')
+        self.assertRaises(TypeError, eab.update_contact, 'string' , 'string' , 3)
+        self.assertRaises(TypeError, eab.update_contact, 'string' , 2 , 'string')
+        self.assertRaises(TypeError, eab.update_contact, 1 , 'string' , 'string')
+        self.assertRaises(TypeError, eab.list_contacts, 'string', 'other string')
+        self.assertRaises(TypeError, eab.search_contact, 1)
+        self.assertRaises(TypeError, eab.has, 1)
+    def test_too_big(self):
+        self.assertRaises(Exception, eab.add_contact, 
+        'lzvicpghimacvfaizmkfkshiivuyjwlqfxnyxxpskpgyznrksjdnurwdtdkxazbisflefdxsahhiyxdviosulmrjwrbrxjntxtytf', 
+        'nmapugnwshpshzzraaupbeuaadwsmoqqkdnvosudhthskvawogwjzqashpaqkiijmnnpwxugcrmkutklpkzwxmkkhnhdlmrrqiildzozmkhmydydcwbculceybbbafjoxtgmcjsjhyfrpbobrafhwii', 
+        '5529533362262178')
+        self.assertRaises(Exception, eab.update_contact, 
+        'lzvicpghimacvfaizmkfkshiivuyjwlqfxnyxxpskpgyznrksjdnurwdtdkxazbisflefdxsahhiyxdviosulmrjwrbrxjntxtytf', 
+        'nmapugnwshpshzzraaupbeuaadwsmoqqkdnvosudhthskvawogwjzqashpaqkiijmnnpwxugcrmkutklpkzwxmkkhnhdlmrrqiildzozmkhmydydcwbculceybbbafjoxtgmcjsjhyfrpbobrafhwii', 
+        '5529533362262178')
+        self.assertRaises(Exception, eab.search_contact, 
+        'lzvicpghimacvfaizmkfkshiivuyjwlqfxnyxxpskpgyznrksjdnurwdtdkxazbisflefdxsahhiyxdviosulmrjwrbrxjntxtytf')
+        self.assertRaises(Exception, eab.delete_contact, 
+        'lzvicpghimacvfaizmkfkshiivuyjwlqfxnyxxpskpgyznrksjdnurwdtdkxazbisflefdxsahhiyxdviosulmrjwrbrxjntxtytf')
     def tearDown(self):
         eab.delete_contact('Zulu Fakename')
+        eab.delete_contact('string')
 if __name__ == '__main__':
     unittest.main()
     
