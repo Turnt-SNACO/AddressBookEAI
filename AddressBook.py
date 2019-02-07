a4b1fb384432b81616edb3a6e9f6685d88aca5ac
Turnt-SNACO <janderson5@albany.edu>
2019-02-07 11:57:35 -0500

    cleaned up html, added ElasticAB layer between flask and Elasticsearch

--- AddressBook.py
+++ AddressBook.py
@@ -3,26 +3,42 @@
 
 from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
+from ElasticAB import ElasticAB
 
 app = Flask(__name__)
-es = Elasticsearch([{'host': 'localhost', 'port' : '9200'}])
-
+eab = ElasticAB()
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
     #display index page
     return render_template('index.html')
 
-
 @app.route('/add', methods=['GET', 'POST'])
 def add_contact():
     #display add form
+    name = request.args.get("name")
+    address = request.args.get("addr")
+    phone_number = request.args.get("phnm")
+    if (name is not None and address is not None and phone_number is not None):
+        success = eab.add_contact(name, address, phone_number)
+        if success:
+            return render_template('addc.html')
+        else:
+            return render_template('addf.html')
     return render_template('add.html')
 
 @app.route('/update', methods=['GET', 'POST'])
 def update_contact():
-    #display update form
+    name = request.args.get("name")
+    address = request.args.get("addr")
+    phone_number = request.args.get("phnm")
+    if (name is not None and address is not None and phone_number is not None):
+        success = eab.update_contact(name, address, phone_number)
+        if success:
+            return render_template('updatec.html')
+        else:
+            return render_template('updatef.html')
+    #display update form~
     return render_template('update.html')
-
 
 @app.route('/list')
 def get_contact_list():
@@ -30,7 +46,13 @@ def get_contact_list():
     return render_template('list.html')
 
 @app.route('/search')
-def get_contact(name):
+def get_contact():
+    name = request.args.get("name")
+    if name is not None:
+        result = eab.search_contact(name)
+        if result is False:
+            return "NO"
+        return render_template('search_res.html', name_r=result.name, addr_r=result.address, phnm_r=result.phone_number)
     #display search form
     return render_template('search.html')
 
---
+++ ElasticAB.py
@@ -0,0 +1,78 @@
+# Author: James Anderson
+# This class interacts with the Elasticsearch data store of contacts
+# The datastore will index by name since name must be a unique field
+# Supported fields are name, address, and phone number
+
+from elasticsearch import Elasticsearch
+INDEX='contacts'
+
+#A container for contact information
+class Contact:
+    def __init__(self, name, address, phone_number):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+
+class ElasticAB:
+    #Will have default of localhost and port 9200 if unspecified
+    def __init__(self, address='localhost', port='9200'):
+        self.address = address
+        self.port = port
+        self.es = Elasticsearch([{'host': address, 'port' : port}])
+
+    #Add a contact if it does not already exist otherwise
+    #Returns boolean value which indicates success or failure
+    def add_contact(self, name, address, phone_number):
+        name = name.lower()
+        if not self.has(name):
+            body = {"name":name,"address":address,"phnm":phone_number}
+            self.es.create(index=INDEX, doc_type='_doc', id=name, body=body)
+            return True
+        return False
+    
+    #Get contact information if it exists
+    #Returns a contact object
+    def search_contact(self, name):
+        name = name.lower()
+        if self.has(name):
+            data = self.es.get(index=INDEX, doc_type='_doc', id=name)['_source']
+            contact = Contact(data['name'], data['address'], data['phnm'])
+            return contact
+        return False
+    
+    #Update existing contact
+    #Returns boolean value which indicates success or failure
+    def update_contact(self, name, address, phone_number):
+        if self.has(name):
+            body = {"name":name,"address":address,"phnm":phone_number}
+            self.es.update(index=INDEX, doc_type='_doc', id=name, body=body)
+            return True
+        return False
+
+    #Delete an existing contact
+    #Returns boolean value which indicates success or failure
+    def delete_contact(self, name):
+        if self.has(name):
+            self.es.delete(index=INDEX, doc_type='_doc', id=name)
+            return True
+        return False
+
+    #List a subset of results in alphabetical order
+    #   epp - entries per page
+    #   page - starts at 0
+    #returns a list of contact objects with results of search
+    def list_contacts(self, epp, page):
+        frm = epp*(page)
+        body = {"query": {"match_all" : {}}}
+        data = self.es.search(index=INDEX, doc_type='_doc', from_=frm, size=epp, body=body)
+        entries = []
+        for i in data['hits']['hits']:
+            entry = Contact(i['_source']['name'], i['_source']['address'], i['_source']['phnm'])
+            entries.append(entry)
+        return entries
+
+    #Shorthand to make typing easier
+    def has(self, name):
+        return self.es.exists(index=INDEX, doc_type='_doc', id=name)
+
+
